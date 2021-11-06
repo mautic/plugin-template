6004c87cc7dfb01e74bd2a02411907a610aa6edc
Dennis Ameling (he/him) <dennis.ameling@leap.ac>
2021-11-06 15:39:37 +0100

    Initial commit

---
+++ _BundleTemplate.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\REPLACE_BUNDLE_NAME;
+
+use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
+
+class REPLACE_BUNDLE_NAME extends AbstractPluginBundle
+{
+}
